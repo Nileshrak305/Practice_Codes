5d55a6708dc75f516f8d3b46d41a5eb3cd0b5d6e
Nileshrak305 <49835180+Nileshrak305@users.noreply.github.com>
2022-08-08 22:33:06 +0530

    Add files via upload
    Basics of OOPS Class and Its Instances

---
+++ Test.java
@@ -0,0 +1,28 @@
+package com.psl.ALPtraining;
+
+class Student{
+	String name;
+	int rollno;
+	public void infodisplay(String name) {
+		System.out.println("Hello "+name);
+	}
+	public void infodisplay(int rollno) {
+		System.out.println("Roll no is "+rollno);
+	}
+	public void infodisplay(String name,int rollno) {
+		System.out.println("Hello "+name+" your Roll no is "+rollno);
+	}
+}
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s = new Student();
+//		s.name = "Nilesh";
+//		s.rollno = 33;
+		s.infodisplay("Nilesh",33);
+		s.infodisplay("Nilesh");
+		s.infodisplay(33);
+	}
+
+}
