33f461e5d3719701ed90a4dd343310e71df67abb
Nileshrak305 <49835180+Nileshrak305@users.noreply.github.com>
2022-08-23 00:28:25 +0530

    commit
    Singly Linked List without collection Framework.

---
+++ LList.java
@@ -0,0 +1,63 @@
+package com.practice1;
+
+public class LList {
+	Node head;
+	
+	class Node{
+		String data;
+		Node next;
+		
+		Node(String data){
+			this.data= data;
+			this.next= null;
+		}
+		
+		
+	}
+	//add first
+	public void addFirst(String data) {
+		Node newNode = new Node(data);
+		if(head==null) {
+			head = newNode;
+			return;
+		}
+		newNode.next=head;
+		head = newNode;
+	}
+	
+	public void addLast(String data) {
+		Node newNode = new Node(data);
+		if(head==null) {
+			head = newNode;
+			return;
+		}
+		Node currNode = head;
+		while(currNode.next!=null) {
+			currNode = currNode.next;
+		}
+		currNode.next = newNode;
+	}
+
+	public void printList() {
+		if(head==null) {
+			System.out.println("List is Empty");
+			return;
+		}
+		Node currNode = head;
+		while(currNode!=null) {
+			System.out.print(currNode.data+"->");
+			currNode = currNode.next;
+		}
+		System.out.println("NULL");
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LList list = new LList();
+		list.addFirst("is");
+		list.addFirst("Nilesh");
+		list.printList();
+}
+}
